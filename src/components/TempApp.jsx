373b9a06a141d6d208b7e31ca484d989db94b538
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-13 18:51:30 +0600

    search weather by inputting city name finished

--- src/components/TempApp.jsx
+++ src/components/TempApp.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./TempApp.css";
 const TempApp = () => {
-  const [city, setCity] = useState(null);
-  const [search, setSearch] = useState("");
-
+  const [city, setCity] = useState("");
+  console.log(city);
+  const [search, setSearch] = useState("Dhaka");
   useEffect(() => {
     const fetchApi = async () => {
       const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c173796eecfe7908ca1c436e70d0cadc`;
       const response = await fetch(url);
       const resJson = await response.json();
-      setCity(resJson);
+      setCity(resJson.main);
     };
     fetchApi();
-  }, []);
+  }, [search]);
   return (
     <>
       <div className="box">
@@ -23,16 +23,23 @@ const TempApp = () => {
             onChange={(event) => {
               setSearch(event.target.value);
             }}
+            placeholder="Enter city name..."
           />
         </div>
-        <div className="info">
-          <h2 className="location">
-            <i className="fa-solid fa-street-view" color="color: #11fd11"></i>
-            {search}
-          </h2>
-          <h1 className="temp">5.25°Cel</h1>
-          <h3>Min : 5.25°Cel | Max : 5.25°Cel</h3>
-        </div>
+        {!city ? (
+          <p className="error">No Data Found</p>
+        ) : (
+          <div className="info">
+            <h2 className="location">
+              <i className="fa-solid fa-street-view" color="color: #11fd11"></i>
+              {search}
+            </h2>
+            <h1 className="temp">{city.temp}°Cel</h1>
+            <h3>
+              Min :{city.temp_min}°Cel | Max :{city.temp_max}°Cel
+            </h3>
+          </div>
+        )}
       </div>
     </>
   );
